410bdcc01ba0c828ea59330df51485ca9375ac5e
jony5ds <37637355+jony5ds@users.noreply.github.com>
2021-07-11 19:39:55 -0300

    Subindo app

---
+++ Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivityTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.leilao.ui.activity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LancesLeilaoActivityTest {
+
+    @Test
+    public void deve_MostrarMensagemDeFalha_QuandoLanceForMenorQueUltimoLance() {
+
+    }
+
+}
